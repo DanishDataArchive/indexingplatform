3af9f8d15303a9167afbd2bdb05245e3c104d215
Jannik V. Jensen <jvj@dda.dk>
2012-01-08 01:14:21 +0000

    Added set get text on element
    git-svn-id: https://subversion.assembla.com/svn/ddaipf/trunk@124 eb421193-52bd-47ae-b03d-ab49450b5660

--- ddi-indexingplatform/src/dk/dda/indexingplatform/util/MetadataBuilder.java
+++ ddi-indexingplatform/src/dk/dda/indexingplatform/util/MetadataBuilder.java
@@ -2,6 +2,10 @@ package dk.dda.indexingplatform.util;
 
 import java.math.BigInteger;
 
+import org.apache.xmlbeans.XmlCursor;
+import org.apache.xmlbeans.XmlObject;
+import org.apache.xmlbeans.XmlCursor.TokenType;
+
 import dk.dda.ddi.indexingplatform.scope.ScopeType;
 import dk.dda.ddi.indexingplatform.searchmetadata.SearchMetaDataType;
 import dk.dda.ddi.indexingplatform.simplesearch.SimpleSearchParametersDocument;
@@ -31,5 +35,70 @@ public class MetadataBuilder {
 		searchMetaData.setHitStart(new BigInteger("0"));
 
 		return doc;
+	}
+	
+	/**
+	 * Retrieve text on a mixed content element
+	 * 
+	 * @param xmlObject
+	 *            to retrieve text from
+	 * @return text
+	 */
+	public static String getTextOnMixedElement(XmlObject xmlObject) {
+		if (xmlObject==null) { // guard
+			return "";
+		}
+		
+		XmlCursor xmlCursor = xmlObject.newCursor();
+		// toLastAttribute does not skip namespaces - so continue
+		// until none empty TEXT token
+		xmlCursor.toLastAttribute();
+		TokenType token = xmlCursor.toNextToken();
+		if (token.equals(TokenType.END) || token.equals(TokenType.ENDDOC)) {
+			return "";
+		}
+
+		String text = null;
+		try {
+			text = xmlCursor.getTextValue().trim();
+		} catch (IllegalMonitorStateException e) {
+			return "";
+		}
+
+		while (!token.equals(XmlCursor.TokenType.TEXT)
+				|| (token.equals(XmlCursor.TokenType.TEXT) && text.length() == 0)) {
+			token = xmlCursor.toNextToken();
+			if (token.equals(XmlCursor.TokenType.END)) {
+				return "";
+			}
+			text = xmlCursor.getTextValue().trim();
+		}
+		xmlCursor.dispose();
+		return text;
+	}
+	
+	/**
+	 * Set text on a mixed content element at first position after attributs
+	 * 
+	 * @param xmlObject
+	 *            to set text on
+	 * @param text
+	 *            to set
+	 * @return altered xml object
+	 */
+	public static XmlObject setTextOnMixedElement(XmlObject xmlObject,
+			String text) {
+		XmlCursor xmlCursor = xmlObject.newCursor();
+
+		// insert new text
+		xmlCursor.toFirstContentToken();
+		xmlCursor.insertChars(text);
+
+		// remove old text
+		String result = xmlCursor.getChars();
+		xmlCursor.removeChars(result.length());
+		xmlCursor.dispose();
+
+		return xmlObject;
 	}
 }
