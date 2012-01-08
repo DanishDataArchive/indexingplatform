d447d77e49f2a9afd95a102913b1316d76b1c50c
Jannik V. Jensen <jvj@dda.dk>
2012-01-08 01:15:10 +0000

    Added study unit title test
    git-svn-id: https://subversion.assembla.com/svn/ddaipf/trunk@125 eb421193-52bd-47ae-b03d-ab49450b5660

--- ddi-indexingplatform/testsrc/dk/dda/indexingplatform/rest/SimpleSearchTest.java
+++ ddi-indexingplatform/testsrc/dk/dda/indexingplatform/rest/SimpleSearchTest.java
@@ -2,10 +2,14 @@ package dk.dda.indexingplatform.rest;
 
 import junit.framework.Assert;
 
+import org.ddialliance.ddieditor.model.lightxmlobject.CustomListType;
+import org.ddialliance.ddieditor.model.lightxmlobject.CustomType;
 import org.ddialliance.ddieditor.model.lightxmlobject.LightXmlObjectListType;
+import org.ddialliance.ddieditor.model.lightxmlobject.LightXmlObjectType;
 import org.junit.Test;
 
 import dk.dda.ddi.indexingplatform.simplesearch.SimpleSearchParametersDocument;
+import dk.dda.indexingplatform.util.CustomListElement;
 import dk.dda.indexingplatform.util.MetadataBuilder;
 
 public class SimpleSearchTest extends Testbase {
@@ -16,7 +20,6 @@ public class SimpleSearchTest extends Testbase {
 				.getSimpleSearchParameters();
 		String studyId = "13794";
 		doc.getSimpleSearchParameters().setSearchString(studyId);
-		// System.out.println(doc);
 
 		LightXmlObjectListType result = restClient.getAsLightXmlObject(
 				RestTarget.SIMPLE_SEARCH, doc.xmlText());
@@ -25,20 +28,59 @@ public class SimpleSearchTest extends Testbase {
 				.getLightXmlObjectList().size());
 		Assert.assertEquals("Not same id!", studyId, result
 				.getLightXmlObjectList().get(0).getId());
-		// System.out.println(result.xmlText());
 	}
 
 	@Test
-	public void searchQuestion() throws Exception {
+	public void searchStudyUnitTitle() throws Exception {
 		SimpleSearchParametersDocument doc = MetadataBuilder
 				.getSimpleSearchParameters();
-		doc.getSimpleSearchParameters().setSearchString("studienummer");
+		doc.getSimpleSearchParameters().setSearchString("karriereforløb");
 
 		LightXmlObjectListType result = restClient.getAsLightXmlObject(
 				RestTarget.SIMPLE_SEARCH, doc.getSimpleSearchParameters()
 						.xmlText());
-		
-		Assert.assertTrue("No results!", result
-				.getLightXmlObjectList().size() > 0);
+
+		Assert.assertTrue("No results!",
+				result.getLightXmlObjectList().size() > 0);
+
+		for (LightXmlObjectType lightXmlObject : result.getLightXmlObjectList()) {
+			for (CustomListType customList : lightXmlObject.getCustomListList()) {
+				if (CustomListElement
+						.getCustomListElement(customList.getType()).equals(
+								CustomListElement.STUDY_UNIT)) {
+					for (CustomType custom : customList.getCustomList()) {
+						if (custom.getOption().equals("id")
+								&& MetadataBuilder
+										.getTextOnMixedElement(custom).equals(
+												"2482")) {
+							return;
+						}
+					}
+				}
+			}
+		}
+		Assert.fail("StudyUnit not found!");
+	}
+	
+	@Test
+	public void searchQuestionItem() throws Exception {
+//		restClient.logQuery=true;
+		SimpleSearchParametersDocument doc = MetadataBuilder
+				.getSimpleSearchParameters();
+		doc.getSimpleSearchParameters().setSearchString("karriere");
+
+		LightXmlObjectListType result = restClient.getAsLightXmlObject(
+				RestTarget.SIMPLE_SEARCH, doc.getSimpleSearchParameters()
+						.xmlText());
+
+		Assert.assertTrue("No results!",
+				result.getLightXmlObjectList().size() > 0);
+		int count =0;
+		for (LightXmlObjectType lightXmlObject : result.getLightXmlObjectList()) {
+			if (lightXmlObject.getElement().equals("QuestionItem")) {
+				count++;
+			}
+		}
+		Assert.assertEquals("Not exact no of question items", 2, count);
 	}
 }
