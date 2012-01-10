b59eb98b858daad29ca6fd98dac57615b8ad0279
Jannik V. Jensen <jvj@dda.dk>
2012-01-10 23:15:53 +0000

    Added two duplication test
    git-svn-id: https://subversion.assembla.com/svn/ddaipf/trunk@162 eb421193-52bd-47ae-b03d-ab49450b5660

--- ddi-indexingplatform/testsrc/dk/dda/indexingplatform/rest/UrnTest.java
+++ ddi-indexingplatform/testsrc/dk/dda/indexingplatform/rest/UrnTest.java
@@ -12,10 +12,38 @@ import dk.dda.indexingplatform.util.MetadataBuilder;
 
 public class UrnTest extends Testbase {
 	@Test
-	public void dublicateElementInDDAUrnCollection() throws Exception {
-		Assert.fail("400 error code returned when same study unit file is loaded into collection!");
+	public void dublicateAbstractInDDAUrnCollection() throws Exception {
+		//Assert.fail("400 error code returned when same study unit file is loaded into collection!");
+		// 
+		String id = "abst-7889222a-5069-48a7-a4ac-b111d4f1bb51";
+		String result = 
+				restClient.getUrn(MetadataBuilder.getUrn(
+				id, "1.0.0"));		
+
+		IdentifiedStructuredStringType type = AbstractDocument.Factory.parse(result).getAbstract();
+		Assert.assertEquals("Not same id!", id, type.getId());
+		System.out.println(MetadataBuilder.getTextOnMixedElement(type.getContent()));	
 	}
-		
+
+	@Test
+	public void dublicateQuestionItemInDDAUrnCollection() throws Exception {
+		//Assert.fail("400 error code returned when same study unit file is loaded into collection!");
+		// 
+		String id = "quei-13b3ae4b-0c86-4813-9276-7684d4441e45";
+		String result=null;
+				
+				try {
+					result = restClient.getUrn(MetadataBuilder.getUrn(
+					id, "1.0.0"));
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}		
+
+		QuestionItemType type = QuestionItemDocument.Factory.parse(result).getQuestionItem();
+		Assert.assertEquals("Not same id!", id, type.getId());	
+	}
+	
 	@Test
 	public void versionableUrn() throws Exception {
 		String id = "quei-11f628da-15b3-4140-8735-a9811bddb733";
