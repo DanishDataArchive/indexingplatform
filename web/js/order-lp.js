86b536a440dd9b5f87baaaf0fa0dc6259ce6a874
Jannik V. Jensen <jvj@dda.dk>
2013-03-05 09:31:30 +0000

    Added search href
    git-svn-id: https://subversion.assembla.com/svn/ddaipf/trunk@376 eb421193-52bd-47ae-b03d-ab49450b5660

--- web/js/order-lp.js
+++ web/js/order-lp.js
@@ -7,15 +7,21 @@ function createOrder() {
     var studyIdElements = document.getElementsByName("studyId[]");
     var studyTitleElements = document.getElementsByName("studyTitle[]");
     var studyIDs = new Array();;
-    var studyTitles = new Array();;        
+    var studyTitles = new Array();;
     
-	// Add the study ID and title to the lists
-	studyIDs.push(studyIdElements[0].value);
-	studyTitles.push(studyTitleElements[0].value);
-
+    // Add the study ID and title to the lists
+    studyIDs.push(studyIdElements[0].value);
+    studyTitles.push(studyTitleElements[0].value);
+    
     // Serialize the arrays to strings and store them into local storage
     localStorage.setItem('studyIDs', JSON.stringify(studyIDs));
     localStorage.setItem('studyTitles', JSON.stringify(studyTitles));
     
-window.open("/order/order.html", "_blank");
+    window.open("/order/order.html", "_blank");
+}
+
+function studySearch(text) {
+    $('#searchform').append('<input type="hidden" name="search-string" value="&quot;'+text+'&quot;"/>');   
+    $('#searchform').append('<input type="hidden" name="StudyUnit" value="true"/>');
+    $('#searchform').submit();
 }
